14208d30a9ce5f295395584a680c101ebb244c51
Shernan Jenesis Mateo <shernanjenesismateo0@yahoo.com>
2023-11-17 09:07:04 +0800

    added springboot

---
+++ src/main/java/OMAS/OfficeTableReservationSystem/OfficeTableReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package OMAS.OfficeTableReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeTableReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeTableReservationSystemApplication.class, args);
+	}
+
+}
