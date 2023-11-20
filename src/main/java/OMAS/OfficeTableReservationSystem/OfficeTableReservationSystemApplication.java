94c320c597dda2e4d1ab6c5c2da57394213bf882
Christian Regienald Jacinto <142294368+Rejiiii@users.noreply.github.com>
2023-11-20 11:29:07 +0800

    Updated login funtion

--- src/main/java/OMAS/OfficeTableReservationSystem/OfficeTableReservationSystemApplication.java
+++ src/main/java/OMAS/OfficeTableReservationSystem/OfficeTableReservationSystemApplication.java
@@ -1,9 +1,11 @@
 package OMAS.OfficeTableReservationSystem;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("OMAS.OfficeTableReservationSystem.dao")
 public class OfficeTableReservationSystemApplication {
 
 	public static void main(String[] args) {
