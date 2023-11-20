97adeeba98974be6bc7cef4d4259809647d28ac5
Shernan Jenesis Mateo <shernanjenesismateo0@yahoo.com>
2023-11-20 09:52:45 +0800

    Fixed the whole frontend folder

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-import { Provider } from "react-redux";
-import store from "./redux/store/store"
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-console.warn(store);
+const container = document.getElementById('root')!;
+const root = createRoot(container);
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
