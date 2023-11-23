891127514ddafd7c29627fc6136c541679701d02
Kekingkyawz19 <142292496+Kekingkyawz19@users.noreply.github.com>
2023-11-23 08:50:02 +0800

    reservation form

--- frontend/src/components/calendar/Calendar.tsx
+++ frontend/src/components/calendar/Calendar.tsx
@@ -1,36 +1,64 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-
+import { Dialog, DialogTitle, DialogContent, TextField, Button } from "@mui/material";
 
 function Calendar() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [startDate, setStartDate] = useState<string | null>(null);
+  const [endDate, setEndDate] = useState<string | null>(null);
+  const [events, setEvents] = useState<any[]>([]); // State to store calendar events
 
   const handleModalOpen = () => {
-    setIsModalOpen(true);
+    setModalOpen(true);
   };
-
 
   const handleModalClose = () => {
-    setIsModalOpen(false);
+    setModalOpen(false);
   };
 
+  const handleDateChange = (value: string, type: 'start' | 'end') => {
+    if (type === 'start') {
+      setStartDate(value);
+    } else if (type === 'end') {
+      setEndDate(value);
+    }
+  };
+
+  const handleAddAppointment = () => {
+    const newEvent = {
+      title: 'Reserved', 
+      start: startDate,
+      end: endDate,
+      allDay: false,
+    };
+
+    setEvents((prevEvents) => [...prevEvents, newEvent]);
+
+    handleModalClose();
+  };
 
   const customButton = {
-    text: '+ Add Appointment',
+    text: '+ Reserve Seat',
     click: handleModalOpen,
   };
-
 
   const headerToolbar = {
     left: 'today,prev,next,title',
     center: '',
-    right: 'timeGridWeek,dayGridMonth,timeGridDay, customButton',
+    right: 'timeGridWeek,dayGridMonth,timeGridDay customButton',
   };
 
+  useEffect(() => {
+    fetch("http://localhost:3001/dates")
+      .then((response) => response.json())
+      .then((data) => {
+        setEvents(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []); 
 
   return (
     <div>
@@ -51,12 +79,40 @@ function Calendar() {
         slotMinTime="06:00:00"
         slotMaxTime="19:00:00"
         customButtons={{ customButton }}
+        events={events}
       />
 
-
+      <Dialog open={isModalOpen} onClose={handleModalClose}>
+        <DialogTitle>Add Reservation</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Start Date"
+            type="datetime-local"
+            onChange={(e) => handleDateChange(e.target.value, 'start')}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            fullWidth
+          />
+          <TextField
+            label="End Date"
+            type="datetime-local"
+            onChange={(e) => handleDateChange(e.target.value, 'end')}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            fullWidth
+          />
+          <Button onClick={handleAddAppointment} variant="contained" color="primary">
+            Add Reservation
+          </Button>
+          <Button onClick={handleModalClose} variant="contained" color="primary">
+            Cancel
+          </Button>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
-
 
 export default Calendar;
